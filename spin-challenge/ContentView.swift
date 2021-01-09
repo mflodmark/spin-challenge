d3c49204a902288c955f644278c3728d309cc47a
Markus Flodmark <markus.flodmark@gmail.com>
2021-01-09 10:37:51 +0100

    Initial Commit

---
+++ spin-challenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  spin-challenge
+//
+//  Created by Markus Flodmark on 2021-01-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ spin-challenge/spin_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  spin_challengeApp.swift
+//  spin-challenge
+//
+//  Created by Markus Flodmark on 2021-01-09.
+//
+
+import SwiftUI
+
+@main
+struct spin_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
